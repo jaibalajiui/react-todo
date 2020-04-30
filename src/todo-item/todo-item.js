d16c8c244092eb759be2b7fa666dc8915ad0c8a6
jai balaji <jai.balaji@cerner.com>
2020-04-30 21:17:39 +0530

    basic todo app

--- src/todo-item/todo-item.js
+++ src/todo-item/todo-item.js
@@ -1,12 +1,59 @@
 import React, { Component } from "react";
+import './todo-item.css';
 
 class TodoItems extends Component {
-    render(){
+
+    constructor(props){
+        super(props);
+        this.state = {
+            itemsList: []
+        }
+        this.item = null;
+        console.log("state: costructor",this.state);
+    }
+
+    changeItemHandler = (event)=>{
+       this.setState({
+           item: event.target.value
+       });
+       console.log("changeItem Handler state",this.state);
+    }
+
+    addItemHandler = (event)=>{
+        this.setState(
+            {
+                itemsList:[...this.state.itemsList,this.state.item],
+                item: ''
+            }
+        );
+        console.log("addItem Handler state: ",this.state);
+    }
+
+    render()
+    {
         return(
             <div className="container"> 
                 <div className="row">
-                    <div className="col-sm-10">
-                    
+                    <div className="col-sm-12">
+                    <h2 className="text-center">To-Do List Application</h2>
+                        <div className="todo-item">
+                            <div className="form-group">
+                                <input className="form-control" placeholder="Enter the item" onChange={this.changeItemHandler} value={this.state.item}/>
+                                <button className="form-control btn btn-primary" onClick={this.addItemHandler}> Add Item </button>
+                            </div> 
+                        </div>
+                        <div>
+                            { this.state.itemsList.length ?
+                                <ul className="list-group todo-list">
+                                    {   
+                                        this.state.itemsList.map((item,index)=>{
+                                            return(<li className="list-group-item" key={index}> {item} </li>)
+                                        })
+                                    }
+                                </ul>
+                            : <div className="text-center">No elements</div>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
